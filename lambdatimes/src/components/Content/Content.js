2a601c73676f6dcd869f945d78f4b63c891bb521
Dstar4 <danielstarling4@gmail.com>
2019-02-22 11:47:47 -0700

    css converted for all but App

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -5,7 +5,13 @@ import Cards from "./Cards";
 
 // Importing our tab and card data. No need to change anything here.
 import { tabData, cardData } from "../../data";
+import styled from "styled-components";
 
+const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 export default class Content extends Component {
   constructor(props) {
     super(props);
@@ -54,7 +60,7 @@ export default class Content extends Component {
   render() {
     console.log("Rendering");
     return (
-      <div className="content-container">
+      <ContentContainer>
         {/*
           Add 2 props to the Tabs component,
           `selectedTab` that includes the currently selected tab
@@ -66,7 +72,7 @@ export default class Content extends Component {
           selectedTabHandler={this.changeSelected}
         />
         <Cards cards={this.filterCards()} />
-      </div>
+      </ContentContainer>
     );
   }
 }
--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,12 +1,32 @@
 import React from "react";
+import propTypes from "prop-types";
+import styled from "styled-components";
 
+const TabStyle = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  color: #fff;
+  background-color: #333;
+  margin: 0 5px;
+  padding: 2px 10px;
+  font-size: 12px;
+  letter-spacing: 2px;
+  cursor: pointer;
+  font-weight: bold;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop,
       if they match, the className should be: 'tab active-tab',
       if it is not it should just be 'tab'*/
   const selected = "tab";
   return (
-    <div
+    <TabStyle
+      // HOW DO YOU STYLE .active-tab???
       className={`tab ${props.selected ? "active-tab" : ""}`}
       onClick={
         () => props.selectedTabHandler(props.tab)
@@ -15,14 +35,14 @@ const Tab = props => {
       }
     >
       {props.tab.toUpperCase()}
-    </div>
+    </TabStyle>
   );
 };
-// Tab.propTypes = {
-//   tab: propTypes.string,
-//   selectedTab: propTypes.string,
-//   selectTabHandler: propTypes.function
-// };
+Tab.propTypes = {
+  tab: propTypes.string,
+  selectedTab: propTypes.string,
+  selectTabHandler: propTypes.function
+};
 // Make sure you include PropTypes on your props.
 
 export default Tab;
